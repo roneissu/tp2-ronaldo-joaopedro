b0292229116c8112b465a538ad197c281fac0d80
João Pedro Klock <jpklock@hotmail.com>
2017-10-08 12:28:54 -0300

    num_pages function v1

---
+++ xv6/num_pages.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "x86.h"
+#include "syscall.h"
+
+int
+num_pages(void){
+
+struct proc *curproc = myproc();
+uint size = curproc->sz;
+// xv6 page size is 4096 bytes
+uint pages = size/4096;
+return pages;
+
+}
